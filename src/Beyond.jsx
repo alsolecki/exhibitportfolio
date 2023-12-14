df3ac76f721164ca50873096e03cd6aceb7ff07e
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-12-13 22:05:04 -0500

    2nd commit

--- src/Beyond.jsx
+++ src/Beyond.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React from 'react'
+import render01 from '/renderings/01-GreatLakesRTC_final_lo.jpg'
+import render02 from '/renderings/Bluegrass_ (2)lo.jpg'
+import render04 from '/renderings/01-Creation_2017-10-31.jpg'
+import render03 from '/renderings/01_west_day.jpg'
+import render05 from '/renderings/LSU_02-Northwing_FINALlo.jpg'
+import render06 from '/renderings/FMVC_FortMonroe_Rendlo.jpg'
 
 const Beyond = () => {
   return (
-    <div className="main-content beyond">
+    <div className="renderings">
+      <img src={render01} alt="GreatLakesRTC rendering" className="render-image" />
+      <img src={render02} alt="bluegrass exhibit" className="render-image" />
+      <img src={render03} alt="central gallery" className="render-image" />
+      <img src={render04} alt="chickasaw gallery" className="render-image" />
+      <img src={render05} alt="lsu gallery rendering" className="render-image" />
+      <img src={render06} alt="fort monroe rendering" className="render-image" />
     </div>
   )
 }
--- src/CourseInfo.jsx
+++ src/CourseInfo.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import drawing01 from '/drawings/HRNM_plan.jpg'
+import drawing02 from '/drawings/HRNM_bubble.jpg'
+import drawing03 from '/drawings/marathonplan-01-01.jpg'
+import drawing05 from '/drawings/WCplan-CD-02.jpg'
+import drawing07 from '/drawings/PHdetail-01.jpg'
+import drawing08 from '/drawings/axo.jpg'
+
 
 const CourseInfo = () => {
   return (
-    <div className="main-content courseinfo">
-     
+    <div className="drawings">
+      <img src={drawing03} alt="concept plan drawing" className="drawing-image" />
+      <img src={drawing05} alt="concept plan drawing" className="drawing-image" />
+      <img src={drawing07} alt="detail drawing" className="drawing-image" />
+      <img src={drawing02} alt="concept plan drawing" className="drawing-image" />
+      <img src={drawing08} alt="detail drawing" className="drawing-image" />
+      <img src={drawing01} alt="concept plan drawing" className="drawing-image" />
     </div>
   )
 }
